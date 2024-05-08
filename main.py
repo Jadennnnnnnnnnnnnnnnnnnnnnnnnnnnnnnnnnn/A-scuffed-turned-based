a5a5a57f33fadc67464a595f302efe390b92908e
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-05-08 09:27:30 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import pygame
 
+tile_size = 32
+
+
+def create_tiles(r, g, b):
+    tile = pygame.Surface((tile_size, tile_size))
+    tile.fill((r, g, b))
+    return tile
+
+
+tile_colors = {
+    grass: create_tiles(0, 255, 0),
+    walls: create_tiles(150, 75, 0),
+    wood: create_tiles(196, 164, 132)
+}
+
+tiles = [grass, walls, wood]
