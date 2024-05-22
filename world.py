7184300a4f65c1386e3a76969957c9333ae53cd2
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-05-22 11:08:47 -0400

    Update world.py

--- world.py
+++ world.py
@@ -3,7 +3,14 @@ import pygame
 
 class StageCreation:
 
-    def __init__(self, stage):
+    def __init__(self, stage, x, y):
+        self.x = x
+        self.y = y
+        self.size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+        self.stage = self.create_list()
+
+    def create_list(self):
         self.stage = []
         world = []
         if stage == 1:
@@ -12,16 +19,26 @@ class StageCreation:
                     self.world.append(line.strip())
             self.stage = [list(line) for line in world]
 
-    def create_stage(self):
+    def image(self, tile):
+        self.tile = tile
+        self.set_image()
+
+    def set_image(self):
+        if self.tile == 0:
+            self.image = pygame.image.load("tile-0-pixilart.png")
+        elif self.tile == 1 or self.tile == 4:
+            self.image = pygame.image.load("tile-1-pixilart.png")
+        elif self.tile == 2:
+            self.image = pygame.image.load("tile-2-pixilart.png")
+            self.image = image.resize((24, 24))
+        elif self.tile == 3:
+            self.image = pygame.image.load("tile-3-pixilart.png")
+            self.image = image.resize((24, 24))
+
+    def draw(self):
         for i in self.stage:
             for j in i:
-                if j == 0:
-                    self.image = pygame.image.load("tile-0-pixilart.png")
-                elif j == 1 or j == 4:
-                    self.image = pygame.image.load("tile-1-pixilart.png")
-                elif j == 2:
-                    self.image = pygame.image.load("tile-2-pixilart.png")
-                    self.image = image.resize((24, 24))
+                self.stage[i][j].set_image(self.stage[i][j][0])
 
 
 
