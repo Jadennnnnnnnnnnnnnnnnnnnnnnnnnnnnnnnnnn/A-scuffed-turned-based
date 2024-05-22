bc1a965ed98ca788012f66fabd5fd2b12d5b9ab1
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-05-21 23:34:57 -0400

    Update world.py

--- world.py
+++ world.py
@@ -1,14 +1,27 @@
 import pygame
 
 
-class stage_creation:
+class StageCreation:
 
     def __init__(self, stage):
+        self.stage = []
+        world = []
         if stage == 1:
-            t = open("Stage 1", "r")
-            self.stage = [line.strip() for line in t]
-            for i in self.stage:
-                [line.strip() for line in i]
+            with open("Stage 1", "r") as t:
+                for line in t:
+                    self.world.append(line.strip())
+            self.stage = [list(line) for line in world]
+
+    def create_stage(self):
+        for i in self.stage:
+            for j in i:
+                if j == 0:
+                    self.image = pygame.image.load("tile-0-pixilart.png")
+                elif j == 1 or j == 4:
+                    self.image = pygame.image.load("tile-1-pixilart.png")
+                elif j == 2:
+                    self.image = pygame.image.load("tile-2-pixilart.png")
+                    self.image = image.resize((24, 24))
 
 
 
