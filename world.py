237981bc3b4d2c1b54c91510a6db02b918d56d3c
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-05-28 09:26:11 -0400

    Update world.py

--- world.py
+++ world.py
@@ -4,20 +4,24 @@ import pygame
 class StageCreation:
 
     def __init__(self, stage, x, y):
+        self.tile = None
+        self.world = None
         self.x = x
         self.y = y
         self.size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+        self.stage = stage
         self.stage = self.create_list()
 
     def create_list(self):
         self.stage = []
         world = []
-        if stage == 1:
+        if self.stage == 1:
             with open("Stage 1", "r") as t:
                 for line in t:
                     self.world.append(line.strip())
             self.stage = [list(line) for line in world]
+        return self.stage
 
     def image(self, tile):
         self.tile = tile
@@ -28,19 +32,18 @@ class StageCreation:
             self.image = pygame.image.load("tile-0-pixilart.png")
         elif self.tile == 1 or self.tile == 4:
             self.image = pygame.image.load("tile-1-pixilart.png")
+            self.image = pygame.transform.scale(self.image, (24, 24))
         elif self.tile == 2:
             self.image = pygame.image.load("tile-2-pixilart.png")
-            self.image = image.resize((24, 24))
+            self.image = pygame.transform.scale(self.image, (24, 24))
         elif self.tile == 3:
             self.image = pygame.image.load("tile-3-pixilart.png")
-            self.image = image.resize((24, 24))
+            self.image = pygame.transform.scale(self.image, (24, 24))
 
     def draw(self):
         for i in self.stage:
             for j in i:
                 self.stage[i][j].set_image(self.stage[i][j][0])
-
-
-
-
-
+                self.x += 24
+            self.x = 0
+            self.y += 24
