c92310593521d4d1eb8d4cf68b18b23f67641cad
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-05-30 08:14:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from world import StageCreation
 import pygame
 
 pygame.init()
-stage = StageCreation(1, 0, 0)
 screen = pygame.display.set_mode((800, 600))
+stage = StageCreation(1, 0, 0)
+clock = pygame.time.Clock()
 
 running = True
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((0, 0, 0))
     stage.draw(screen)
-    pygame.display.update()
+    pygame.display.flip()
 
 pygame.quit()
