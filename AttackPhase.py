d0c2862fb5293af357d6044eb4555e7f5e96ada2
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-06-12 06:35:24 -0400

    Update AttackPhase.py

--- AttackPhase.py
+++ AttackPhase.py
@@ -4,15 +4,16 @@ import random
 class Damage:
     @staticmethod
     def calculate_damage(attacker, target):
-        damage = round(((random.randint(85, 105) / 100) * attacker.attack) / target.defense * random.randint(5, 10))
+        damage = round(((random.randint(85, 105) / 100) * attacker.attack_power) / target.defense * random.randint(15, 25))
         if random.randint(attacker.crit_rate, 100) <= attacker.crit_rate:
             damage = round(damage * ((random.randint(attacker.crit_damage - 10, attacker.crit_damage + 10) / 100) * target.defense / 3))
+            print("CRIT")
         return damage
 
     @staticmethod
     def attack(attacker, target):
         damage = Damage.calculate_damage(attacker, target)
-        target.damage(damage)
+        target.take_damage(damage)
 
     @staticmethod
     def damage(target, damage):
